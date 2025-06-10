abd13a11af55ca175fa3ea3f3e3fcf3f674eae92
Grakri123 <kgranli-99@live.no>
2025-06-10 16:17:45 +0200

    Oppdatert til mer moderne praksiser

--- install.sh
+++ install.sh
@@ -13,36 +13,24 @@ echo "Installing dependencies..."
 echo "=========================="
 # First update and install system packages
 apt update
-apt -y install python3 python3-pip python3-pygame supervisor mpv ntfs-3g exfat-fuse python3-venv
+apt -y install python3 python3-pip python3-venv python3-pygame supervisor mpv ntfs-3g exfat-fuse
 apt -y install python3-dev python3-setuptools
 apt -y install python3-rpi.gpio python3-pigpio python3-gpiozero
 apt -y install raspi-gpio pigpio
 
-if [ "$*" != "no_hello_video" ]
-then
-	echo "Installing hello_video..."
-	echo "========================="
-	apt -y install git build-essential python3-dev
-	git clone https://github.com/adafruit/pi_hello_video
-	cd pi_hello_video
-	./rebuild.sh
-	cd hello_video
-	make install
-	cd ../..
-	rm -rf pi_hello_video
-else
-    echo "hello_video was not installed"
-    echo "=========================="
-fi
+# Start GPIO daemon
+systemctl enable pigpiod
+systemctl start pigpiod
 
 echo "Installing video_looper program..."
 echo "=================================="
 
-# change the directoy to the script location
+# change the directory to the script location
 cd "$(dirname "$0")"
 
-mkdir -p /mnt/usbdrive0 # This is very important if you put your system in readonly after
-mkdir -p /home/KT/video # create default video directory
+# Create required directories
+mkdir -p /mnt/usbdrive0
+mkdir -p /home/KT/video
 
 # Create group if it doesn't exist and set ownership
 groupadd -f KT
@@ -50,22 +38,18 @@ usermod -a -G KT KT
 usermod -a -G gpio KT
 chown -R KT:KT /home/KT/video
 
-# Start GPIO daemon
-systemctl enable pigpiod
-systemctl start pigpiod
-
 # Create and activate virtual environment
 VENV_PATH="/home/KT/video_looper_env"
-python3 -m venv $VENV_PATH --system-site-packages
+python3 -m venv $VENV_PATH
 chown -R KT:KT $VENV_PATH
 
-# Activate virtual environment and install packages
-. $VENV_PATH/bin/activate
-pip3 install --upgrade pip setuptools wheel
-pip3 install RPi.GPIO pigpio
-# Install the video looper package using pip in editable mode
-pip3 install -e .
+# Install packages in virtual environment
+su - KT << EOF
+source $VENV_PATH/bin/activate
+python3 -m pip install --upgrade pip setuptools wheel
+python3 -m pip install .
 deactivate
+EOF
 
 # Create service file for supervisor
 cat > /etc/supervisor/conf.d/video_looper.conf << EOF
@@ -80,11 +64,17 @@ redirect_stderr=true
 environment=PYTHONPATH=/usr/lib/python3/dist-packages:%(ENV_PYTHONPATH)s
 EOF
 
+# Copy configuration file
 cp ./assets/video_looper.ini /boot/video_looper.ini
 
 echo "Configuring video_looper to run on start..."
 echo "==========================================="
 
-service supervisor restart
+# Ensure log file exists and has correct permissions
+touch /var/log/video_looper.log
+chown KT:KT /var/log/video_looper.log
+
+# Restart supervisor
+systemctl restart supervisor
 
 echo "Finished!"
--- setup.py
+++ setup.py
@@ -8,25 +8,32 @@ setup(
     description='Application to turn your Raspberry Pi into a dedicated looping video playback device, good for art installations, information displays, or just playing cat videos all day.',
     license='GNU GPLv2',
     url='https://github.com/adafruit/pi_video_looper',
+    packages=find_packages(),
+    python_requires='>=3.7',
     install_requires=[
         'pyudev>=0.23.1',
-        'pygame',
+        'pygame>=2.0.0',
         'six>=1.16.0',
-        'RPi.GPIO',
-        'pigpio'
+        'RPi.GPIO>=0.7.0',
+        'pigpio>=1.78'
     ],
-    packages=find_packages(),
-    python_requires='>=3.6',
+    entry_points={
+        'console_scripts': [
+            'video_looper=Adafruit_Video_Looper.video_looper:main',
+        ],
+    },
     classifiers=[
         'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
         'Intended Audience :: End Users/Desktop',
         'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
         'Operating System :: POSIX :: Linux',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
         'Topic :: Multimedia :: Video :: Display',
     ],
 )
